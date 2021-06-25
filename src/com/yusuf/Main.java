547aa60a1a4d995be5b935d698366139ce3491a0
Yusuf Ismail <yusufgamer222@gmail.com>
2021-06-25 16:24:32 +0100

    doings some testing

--- src/com/yusuf/Main.java
+++ src/com/yusuf/Main.java
@@ -1,8 +1,16 @@
 package com.yusuf;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Whats is your name?");
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+
+        System.out.println("Hello, " + name + "!");
+
     }
 }

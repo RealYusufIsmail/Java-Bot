a12111b747c3139b9fec7c4f9a5ce7796fef37de
Yusuf Ismail <yusufgamer222@gmail.com>
2021-07-04 19:21:16 +0100

    added some math

---
+++ src/com/yusuf/Math.java
@@ -0,0 +1,8 @@
+package com.yusuf;
+
+public class Math {
+    public static void main(String[] args) {
+        System.out.println(2 + (-3) + 5 + (-8));
+        System.out.println ((9 * 10) * (20/5));
+    }
+}

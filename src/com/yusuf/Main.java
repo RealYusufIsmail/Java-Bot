2d1efb3220c715bb3edc25b40fb1a29409e7a8c9
Yusuf Ismail <yusufgamer222@gmail.com>
2021-07-20 22:54:59 +0100

    Update Main.java

--- src/com/yusuf/Main.java
+++ src/com/yusuf/Main.java
@@ -3,10 +3,12 @@ package com.yusuf;
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello! My name is Java Bot.");
+        System.out.println("Hello! My name is Yusuf Bot.");
         System.out.println("I was created in 2021.");
         System.out.println("Please, remind me your name.");
 
@@ -14,21 +16,22 @@ public class Main {
         String name = scanner.next();
 
         System.out.println("What a great name you have, " + name + "!");
+        String jokeQuestion = "Would you like for me to say a joke";
+        String answerSample = "Please reply with yes or no";
+        System.out.println(jokeQuestion);
+        System.out.println(answerSample);
 
-        System.out.println("I can do math. Want to see, " + name + "?");
-        String yes = scanner.next();
-        String no = scanner.next();
+        String Value = scanner.next();
+        if (Value.equals("no")) {
+            System.out.println("Well there might be a next time.");
+        } else if((Value.equals("yes"))){
+            System.out.println("Well i one said How is thy. People found it funny.");
+            // yep
+        };
+        String goodBye = "Goodbye have a nice day";
+        System.out.println(goodBye);
 
-        String input = scanner.next();
-        if (input.equalsIgnoreCase(yes)) {
-         // do yes stuff
-            System.out.println("test");
-        } else if (input.equalsIgnoreCase(no)) {
-            // do no stuff
-            System.out.println("Oh well there is always a next time");
-        } else {
-            System.out.println("error");
-        }
+
 
 
     }
